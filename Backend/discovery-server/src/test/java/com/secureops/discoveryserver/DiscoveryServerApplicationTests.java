7c466620bdf99b71a82379525eb437ea4129af5c
Vishwa-coder-06 <vishwam776@gmail.com>
2026-08-05 11:45:58 +0530

    backend started and created the foundation like discoveryserver and api gate way basic

---
+++ Backend/discovery-server/src/test/java/com/secureops/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secureops.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

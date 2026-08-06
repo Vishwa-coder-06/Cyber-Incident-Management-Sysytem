2c64808b321a5cb631c9eacad2a67eec66a6004c
Vishwa-coder-06 <vishwam776@gmail.com>
2026-08-06 11:37:32 +0530

    Minor changes Done

---
+++ Backend/knowledge-service/src/main/java/com/secureops/knowledgeservice/KnowledgeServiceApplication.java
@@ -0,0 +1,18 @@
+package com.secureops.knowledgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class KnowledgeServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(KnowledgeServiceApplication.class,args);
+
+    }
+
+}
+
---
+++ Backend/knowledge-service/src/test/java/com/secureops/knowledgeservice/KnowledgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secureops.knowledgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
